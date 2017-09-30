db02f9dc8c593db68d63fca6cdf66b49527e1b9a
matsuhisa <matsuhisa+github@gmail.com>
2017-09-30 14:05:36 +0900

    Refactoring semver_spec

--- spec/lib/semver_spec.rb
+++ spec/lib/semver_spec.rb
@@ -1,3 +1,4 @@
+
 describe Semver do
   describe "#initialize" do
     it  do
@@ -7,14 +8,22 @@ describe Semver do
   end
 
   describe "#to_s" do
-    it do
-      actual = Semver.new(1, 4, 2)
-      expect(actual.to_s).to eq "1.4.2"
+    subject { Semver.new( major, minor, patch ).to_s }
+
+    context "1.4.2" do
+      let(:major){ 1 }
+      let(:minor){ 4 }
+      let(:patch){ 2 }
+
+      it {is_expected.to eq "1.4.2"}
     end
 
-    it do
-      actual = Semver.new(2, 1, 0)
-      expect(actual.to_s).to eq "2.1.0"
+    context "2.1.0" do
+      let(:major){ 2 }
+      let(:minor){ 1 }
+      let(:patch){ 0 }
+
+      it {is_expected.to eq "2.1.0"}
     end
   end
 end
