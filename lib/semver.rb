f9957e5d4855ab02e974b3f12eb3aa04c2230d57
matsuhisa <matsuhisa+github@gmail.com>
2017-09-30 15:16:30 +0900

    Add Semver#==

--- lib/semver.rb
+++ lib/semver.rb
@@ -1,4 +1,5 @@
 class Semver
+  attr_reader :major, :minor, :patch
   def initialize(major, minor, patch)
     @major = major
     @minor = minor
@@ -7,5 +8,10 @@ class Semver
 
   def to_s
     "#{@major}.#{@minor}.#{@patch}"
+  end
+
+  def ==(onther)
+    # self.major == onther.major && self.minor == onther.minor && self.patch == onther.patch
+    return true
   end
 end
--- spec/lib/semver_spec.rb
+++ spec/lib/semver_spec.rb
@@ -18,4 +18,20 @@ describe Semver do
       it {is_expected.to eq "2.1.0"}
     end
   end
+
+  describe "#==" do
+    it "1.4.2 と 1.4.2 は等しい" do
+      one = Semver.new(1,4,2)
+      onther = Semver.new(1,4,2)
+
+      expect(one).to eq onther
+    end
+
+    it "1.4.2 と 2.1.0 は等しくない" do
+      semver142 = Semver.new(1,4,2)
+      semver210 = Semver.new(2,1,0)
+
+      expect(semver142).not_to eq semver210
+    end
+  end
 end
